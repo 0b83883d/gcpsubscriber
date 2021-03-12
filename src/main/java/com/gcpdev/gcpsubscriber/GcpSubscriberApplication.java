aff4e9d6c4aa06e41229bbfe32726182eb15ff86
PARUL <parul@0b83883d.local>
2021-03-12 10:09:29 +0530

    initial commit

---
+++ src/main/java/com/gcpdev/gcpsubscriber/GcpSubscriberApplication.java
@@ -0,0 +1,47 @@
+package com.gcpdev.gcpsubscriber;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+
+@SpringBootApplication
+public class GcpSubscriberApplication {
+
+	private static final Log LOGGER = LogFactory.getLog(GcpSubscriberApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSubscriberApplication.class, args);
+	}
+
+	@Bean
+	public PubSubInboundChannelAdapter messageChannelAdapter(
+			@Qualifier("myInputChannel") MessageChannel inputChannel,
+			PubSubTemplate pubSubTemplate) {
+		PubSubInboundChannelAdapter adapter =
+				new PubSubInboundChannelAdapter(pubSubTemplate, "mytopic-sub");
+		adapter.setOutputChannel(inputChannel);
+
+		return adapter;
+	}
+
+	@Bean
+	public MessageChannel myInputChannel() {
+		return new DirectChannel();
+	}
+
+
+	@ServiceActivator(inputChannel = "myInputChannel")
+	public void messageReceiver(String payload) {
+		LOGGER.info("Message arrived! Payload: " + payload);
+
+	}
+
+}
